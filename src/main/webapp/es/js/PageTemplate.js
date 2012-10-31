1a9b602c088f11bced701f403f5111c4fdee62a5
Kyle Lahnakoski <klahnakoski@mozilla.com>
2012-10-31 09:12:58 -0400

    hourly commit

---
+++ src/main/webapp/es/js/PageTemplate.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	jQuery.get('file:///C:/Users/klahnakoski/git/Bugzilla%20Anthropology/src/main/webapp/es/PageTemplate.html', function(data) {
+		return data;
+	});
+});
