018fa75ee7af2c1119af4b71854d224d7f72e4a8
Kyle Lahnakoski <klahnakoski@mozilla.com>
2012-10-31 10:12:58 -0400

    hourly commit

--- src/main/webapp/es/js/PageTemplate.js
+++ src/main/webapp/es/js/PageTemplate.js
@@ -1,5 +1,37 @@
+
+
+var readfile=function(path, successFunction){
+	//DECODE RELATIVE PATHS
+//	document.path
+
+	var client = new XMLHttpRequest();
+	client.open('GET', path);
+	client.onreadystatechange = function(state) {
+		successFunction(client.responseText);
+	};
+	client.send();
+};
+
+
+var scriptState={};
+var importScript=function(path){
+
+	if (scriptState[path]=="loaded") return;
+	if (scriptState[path]=="pending") D.error("Import dependency loop detected");
+
+	scriptState[path]="pending";
+	readfile(path, function(content){
+		eval(content);
+		scriptState[path]="loaded";
+	})
+};
+
+
+importScript("lib/js/jquery-1.7.js");
+
+
 $(document).ready(function(){
-	jQuery.get('file:///C:/Users/klahnakoski/git/Bugzilla%20Anthropology/src/main/webapp/es/PageTemplate.html', function(data) {
-		return data;
+	jQuery.get('PageTemplate.html', function(data, success, raw) {
+		$("body").append(raw.responseText);
 	});
 });
