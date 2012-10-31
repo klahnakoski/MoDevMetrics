9eeac10af88c34a54c6b09fa3c25cd40e4dd9e81
Kyle Lahnakoski <klahnakoski@mozilla.com>
2012-10-31 11:12:58 -0400

    hourly commit

--- src/main/webapp/es/js/PageTemplate.js
+++ src/main/webapp/es/js/PageTemplate.js
@@ -7,22 +7,49 @@ var readfile=function(path, successFunction){
 	var client = new XMLHttpRequest();
 	client.open('GET', path);
 	client.onreadystatechange = function(state) {
+		if (this.readyState!=4) return;
 		successFunction(client.responseText);
 	};
 	client.send();
 };
 
 
-var scriptState={};
+var getFullPath=function(currentFullPath, path){
+	var e=path.lastIndexOf("/");
+	if (path.charAt(0)=='/'){
+		if (e==-1) return "/";
+		return path.substring(0, e);
+	}else{
+		if (e==-1) return currentFullPath+"/"+path;
+		return currentFullPath+"/"+path.substring(0, e);
+	}//endif
+};
+
+var scriptLoadStates={};
+
+var scripts = document.getElementsByTagName("script");
+var src = scripts[scripts.length-1].getAttribute("src");
+var scriptPath=getFullPath(getFullPath("", window.location.pathname), src);
+
 var importScript=function(path){
 
-	if (scriptState[path]=="loaded") return;
-	if (scriptState[path]=="pending") D.error("Import dependency loop detected");
+	if (scriptLoadStates[path]=="loaded") return;
+	if (scriptLoadStates[path]=="pending") D.error("Import dependency loop detected");
 
-	scriptState[path]="pending";
-	readfile(path, function(content){
-		eval(content);
-		scriptState[path]="loaded";
+	scriptLoadStates[path]="pending";
+	readfile(scriptPath+"/"+path, function(content){
+		var currPath=scriptPath;
+		scriptPath=getFullPath(scriptPath, path);
+		
+		try{
+			eval(content);
+		}catch(e){
+			scriptPath=currPath;
+			if (D===undefined) throw e;
+			D.error("Can not load script "+path, e);
+		}//try
+		scriptPath=currPath;
+		scriptLoadStates[path]="loaded";
 	})
 };
 
