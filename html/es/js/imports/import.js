4009eba3f20e8a3889370cded74ccf46ab3041d8
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-03-31 14:30:56 -0400

    Fix overall

--- html/es/js/imports/import.js
+++ html/es/js/imports/import.js
@@ -19,7 +19,7 @@ var importScript;
 (function () {
 
 	var METHOD_NAME = "importScript";
-	var FORCE_RELOAD = false;
+	var FORCE_RELOAD = true;
 	var DEBUG = false;
 
 	if (typeof(window.Log) == "undefined") {
