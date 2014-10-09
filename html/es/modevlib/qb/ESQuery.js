0cfffa0fabff537f1cda1c5f25d063d940bcdd2a
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-10-09 12:53:29 -0400

    More error reporting Lock date range to collect Benchmarks always force reload

--- html/es/modevlib/qb/ESQuery.js
+++ html/es/modevlib/qb/ESQuery.js
@@ -649,6 +649,10 @@ ESQuery.INDEXES.bug_dependencies.alternate = ESQuery.INDEXES.public_bug_dependen
 
 		var output = [];
 		var partitions = edge.domain.partitions;
+		if (partitions.length==0){
+			Log.error("There are no partitions in edge "+edge.name+", which is destined for a facet, which wil result in nothing")
+		}//endif
+
 		for (var i = 0; i < partitions.length; i++) {
 			var deeper = this.getAllEdges(edgeDepth + 1);
 			for (var o = 0; o < deeper.length; o++) {
