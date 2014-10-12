b2f7c6aa920d8fb360731a90a23b70d546fe79dd
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-10-12 14:40:36 -0400

    simpler

--- tests/test_get_div.py
+++ tests/test_get_div.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 
 def test_one_page():
-    driver = BetterDriver(webdriver.Firefox())
+    driver = webdriver.Firefox()
     driver.get(File("html/test.html").abspath)
 
     logs = wait_for_logs(driver)
@@ -13,7 +13,7 @@ def test_one_page():
     # print messages
     try:
         for l in logs:
-            if l["message"].find("Hello World") >= 0:
+            if l.find("Hello World") >= 0:
                 break
         else:
             assert False
@@ -23,29 +23,11 @@ def test_one_page():
 
 def wait_for_logs(driver):
     old_length = -1
-    elements = driver.find("#test p")
+    elements = driver.find_elements_by_css_selector("#test p")
     while len(elements) != old_length:
         Thread.sleep(seconds=10)
         old_length = len(elements)
-        elements = driver.find("#test p")
+        elements = driver.find_elements_by_css_selector("#test p")
 
     return [e.text for e in elements]
 
-
-class BetterDriver(object):
-    def __init__(self, driver):
-        self.driver = driver
-
-    def find(self, selector):
-        try:
-            return self.driver.find_elements_by_css_selector(selector)
-        except Exception, e:
-            return []
-
-    def get(self, *args, **kwargs):
-        self.driver.get(*args, **kwargs)
-        return self
-
-    def close(self, *args, **kwargs):
-        self.driver.close(*args, **kwargs)
-        return self
