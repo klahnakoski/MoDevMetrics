0df2f753db5265dab5f568a7e0d162ecf0cec74b
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-10-12 14:54:08 -0400

    simpler, still does not work

--- tests/test_get_div.py
+++ tests/test_get_div.py
@@ -8,26 +8,17 @@ def test_one_page():
     driver = webdriver.Firefox()
     driver.get(File("html/test.html").abspath)
 
-    logs = wait_for_logs(driver)
+    elements = driver.find_elements_by_css_selector("#test p")
 
     # print messages
     try:
-        for l in logs:
+        for l in [e.text for e in elements]:
             if l.find("Hello World") >= 0:
-                break
+                assert True
+                return
         else:
             assert False
     finally:
         driver.close()
 
-
-def wait_for_logs(driver):
-    old_length = -1
-    elements = driver.find_elements_by_css_selector("#test p")
-    while len(elements) != old_length:
-        Thread.sleep(seconds=10)
-        old_length = len(elements)
-        elements = driver.find_elements_by_css_selector("#test p")
-
-    return [e.text for e in elements]
 
